8908313ad65d9f722da5c04462ca88ca88c5d5a1
Nick Playfair <nplayfair@protonmaill.com>
2016-04-05 15:53:16 +0000

    3 LEDs

--- FlashLED.py
+++ FlashLED.py
@@ -6,11 +6,23 @@ GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
 GPIO.setup(12,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
 
 while True:
+	# First LED on
 	GPIO.output(11,True)
 	GPIO.output(12,False)
-	time.sleep(0.5)
+	GPIO.output(13,False)
+	time.sleep(0.33)
+
+	# Second LED on
 	GPIO.output(11,False)
 	GPIO.output(12,True)
-	time.sleep(0.5)
+	GPIO.output(13,False)
+	time.sleep(0.33)
+
+	# Third LED on
+	GPIO.output(11,False)
+        GPIO.output(12,False)
+        GPIO.output(13,True)
+        time.sleep(0.33)
