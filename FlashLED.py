acbaeba8e9b82f14c11669e58a0e4497aa5148db
Nick Playfair <nplayfair@protonmaill.com>
2016-04-05 17:59:35 +0000

    Four LEDs

--- FlashLED.py
+++ FlashLED.py
@@ -1,8 +1,8 @@
 import time
 import RPi.GPIO as GPIO
 
-lights = [11, 12, 13]
-delay = 0.25
+lights = [11, 12, 13, 15]
+delay = 0.08
 
 GPIO.setwarnings(False)
 GPIO.cleanup(lights)
@@ -10,7 +10,11 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
 GPIO.setup(12,GPIO.OUT)
 GPIO.setup(13,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
 
+# Quick Test
+GPIO.output(lights,1)
+time.sleep(1)
 GPIO.output(lights,0)
 
 while True:
@@ -18,22 +22,40 @@ while True:
 	GPIO.output(11,1)
 	GPIO.output(12,0)
 	GPIO.output(13,0)
+	GPIO.output(15,0)
 	time.sleep(delay)
 
 	# Second LED on
 	GPIO.output(11,0)
 	GPIO.output(12,1)
 	GPIO.output(13,0)
+	GPIO.output(15,0)
 	time.sleep(delay)
 
 	# Third LED on
 	GPIO.output(11,0)
         GPIO.output(12,0)
         GPIO.output(13,1)
+        GPIO.output(15,0)
+	time.sleep(delay)
+
+	# Fourth LED on
+        GPIO.output(11,0)
+        GPIO.output(12,0)
+        GPIO.output(13,0)
+        GPIO.output(15,1)
+        time.sleep(delay)
+
+	# Third LED on   
+        GPIO.output(11,0)
+        GPIO.output(12,0)
+        GPIO.output(13,1)
+        GPIO.output(15,0)
         time.sleep(delay)
 
 	# Second LED on
         GPIO.output(11,0)
         GPIO.output(12,1)
         GPIO.output(13,0)
+	GPIO.output(15,0)
         time.sleep(delay)	
