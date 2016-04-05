79fe405bd5e098ddf95aa6ce1b8ef04372f28f55
Nick Playfair <nplayfair@protonmaill.com>
2016-04-05 16:27:09 +0000

    Correct order

--- FlashLED.py
+++ FlashLED.py
@@ -1,28 +1,39 @@
 import time
 import RPi.GPIO as GPIO
 
+lights = [11, 12, 13]
+delay = 0.25
+
 GPIO.setwarnings(False)
-GPIO.cleanup()
+GPIO.cleanup(lights)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
 GPIO.setup(12,GPIO.OUT)
 GPIO.setup(13,GPIO.OUT)
 
+GPIO.output(lights,0)
+
 while True:
 	# First LED on
-	GPIO.output(11,True)
-	GPIO.output(12,False)
-	GPIO.output(13,False)
-	time.sleep(0.33)
+	GPIO.output(11,1)
+	GPIO.output(12,0)
+	GPIO.output(13,0)
+	time.sleep(delay)
 
 	# Second LED on
-	GPIO.output(11,False)
-	GPIO.output(12,True)
-	GPIO.output(13,False)
-	time.sleep(0.33)
+	GPIO.output(11,0)
+	GPIO.output(12,1)
+	GPIO.output(13,0)
+	time.sleep(delay)
 
 	# Third LED on
-	GPIO.output(11,False)
-        GPIO.output(12,False)
-        GPIO.output(13,True)
-        time.sleep(0.33)
+	GPIO.output(11,0)
+        GPIO.output(12,0)
+        GPIO.output(13,1)
+        time.sleep(delay)
+
+	# Second LED on
+        GPIO.output(11,0)
+        GPIO.output(12,1)
+        GPIO.output(13,0)
+        time.sleep(delay)	
