02fa459de8f624031cf315478c2632a2b85617a6
Nick Playfair <nplayfair@protonmaill.com>
2016-04-05 15:42:13 +0000

    Alternate flashing

--- FlashLED.py
+++ FlashLED.py
@@ -1,9 +1,16 @@
 import time
 import RPi.GPIO as GPIO
 
+GPIO.setwarnings(False)
 GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(0,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(12,GPIO.OUT)
 
 while True:
-	GPIO.output(7,True)
+	GPIO.output(11,True)
+	GPIO.output(12,False)
+	time.sleep(0.5)
+	GPIO.output(11,False)
+	GPIO.output(12,True)
+	time.sleep(0.5)
