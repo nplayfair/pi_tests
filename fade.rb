39f45ea8ba6b17111449e40f9f2dbc3cda237fc3
Nick Playfair <nplayfair@protonmaill.com>
2016-04-06 17:04:52 +0000

    Get soft_pwm working

--- fade.rb
+++ fade.rb
@@ -6,22 +6,23 @@ require 'wiringpi2'
 led = 1
 
 # New GPIO obj
-io = WiringPi::GPIO.new do |gpio|
-	gpio.pin_mode(led, WiringPi::PWM_OUTPUT)
-end
+io = WiringPi::GPIO.new
+io.pin_mode(led, WiringPi::PWM_OUTPUT)
+
+led = io.soft_pwm_create 0, 0, 100
 
 # Fading in
 
 loop do
-	for led_level in 0..1023
-	  io.pwmWrite led, led_level
+	for led_level in 0..100
+	  io.soft_pwm_write led, led_level
 	  sleep 0.002
 	end
 
 # Fading out
 
-	for led_level in 0..1023
-	  io.pwmWrite led, 1023 - led_level
+	for led_level in 0..100
+	  io.soft_pwm_write led, 100 - led_level
 	  sleep 0.002
 	end
 end
