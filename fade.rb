527decc4393822dd7a40669953dc4097810d139d
Nick Playfair <nplayfair@protonmaill.com>
2016-04-06 17:19:32 +0000

    Alternate fading

--- fade.rb
+++ fade.rb
@@ -14,20 +14,22 @@ io2 = WiringPi::GPIO.new
 io2.pin_mode(led2, WiringPi::PWM_OUTPUT)
 
 led1 = io1.soft_pwm_create 0, 0, 100
-led2 = io2.soft_pwm_create 1, 0, 100
+led2 = io2.soft_pwm_create 1, 100, 100
 
 # Fading in
 
 loop do
 	for led_level in 0..100
-	  io1.soft_pwm_write led1, led_level
+	  io1.soft_pwm_write 0, led_level
+	  io2.soft_pwm_write 1, 100 - led_level
 	  sleep 0.002
 	end
 
 # Fading out
 
 	for led_level in 0..100
-	  io1.soft_pwm_write led1, 100 - led_level
+	  io1.soft_pwm_write 0, 100 - led_level
+	  io2.soft_pwm_write 1, led_level
 	  sleep 0.002
 	end
 end
