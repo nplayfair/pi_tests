67d30996b256dd4fd3a90529d5075c432ec82965
Nick Playfair <nplayfair@protonmaill.com>
2016-04-07 21:24:01 +0000

    Testing

--- fade_button.rb
+++ fade_button.rb
@@ -6,7 +6,7 @@ require 'wiringpi2'
 # Setup Pins
 LED_PINS = [0, 1, 2]
 BUTTON_PIN = 8
-FADE_VAL = 0.005
+FADE_VAL = 0.003
 
 # Instantiate GPIO object
 io = WiringPi::GPIO.new
@@ -18,22 +18,42 @@ end
 
 # Set parameters
 LED_PINS.each do |led|
-  #io.soft_pwm_create led, 100, 100 if led == 0
-  #puts "#{led} \n"
   io.soft_pwm_create led, 0, 100
 end
 
-# Fade funcion
+## Fade on
+def fade_on (io = io, led)
+  for led_level in 0..100
+    io.soft_pwm_write led, led_level
+    sleep FADE_VAL
+  end
+end
+
+## Fade off
+def fade_off (io = io, led)
+  for led_level in 0..100
+    io.soft_pwm_write led, 100 - led_level
+    sleep FADE_VAL
+  end
+end
+
+## Fade funcion
 def fade (io)
 
 # Vars
 led_count = LED_PINS.length
+state = 1
 on_led = 0
 
 # Wait for button to be pressed and fade LEDs
   loop do
     state = io.digital_read BUTTON_PIN
     if state == 0 # Run the routine
+      # Fade first LED on
+      for led_level in 0..100
+        io.soft_pwm_write on_led, led_level
+      end
+      # Iterate through all LEDs  
       led_count.times do |cycle|
         # Fade on
         for led_level in 0..100
@@ -44,9 +64,21 @@ on_led = 0
           end
         end
         on_led = cycle
+        puts "#{cycle} \n"
+      end # end cycle
+      # Fade last one out
+      for led_level in 0..100
+        io.soft_pwm_write on_led, 100 - led_level
       end
-    end
-  end
-end
+      puts "turn last off\n"
+    end # end if
+  end # end loop
+end # end method
 
-fade io
+#fade io
+fade_on io, 0
+fade_off io, 0
+fade_on io, 1
+fade_off io, 1 
+fade_on io, 2
+fade_off io, 2 
