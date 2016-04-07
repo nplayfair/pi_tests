c83a8e3963b0eb740a2c8c89575b7b030f68cd7c
Nick Playfair <nplayfair@protonmaill.com>
2016-04-07 16:06:28 +0000

    Start fade button script

---
+++ fade_button.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'wiringpi2'
+
+# Fade funcion
+def fade
+
+	# Setup Pins
+	led1 = 0
+	led2 = 1
+	fade_val = 0.003
+
+	io1 = WiringPi::GPIO.new
+	io1.pin_mode(led1, WiringPi::PWM_OUTPUT)
+
+	io2 = WiringPi::GPIO.new
+	io2.pin_mode(led2, WiringPi::PWM_OUTPUT)
+
+	led1 = io1.soft_pwm_create 0, 0, 100
+	led2 = io2.soft_pwm_create 1, 100, 100
+
+	# Fade
+	for led_level in 0..100
+	  io1.soft_pwm_write 0, led_level
+	  io2.soft_pwm_write 1, 100 - led_level
+	  sleep fade_val
+	end
+
+	for led_level in 0..100
+	  io1.soft_pwm_write 0, 100 - led_level
+	  io2.soft_pwm_write 1, led_level
+	  sleep fade_val
+	end
+end
+
+fade
