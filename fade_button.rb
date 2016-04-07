cf97ebf8a4e5ad192bfbf8dac74a25ff134bb49d
Nick Playfair <nplayfair@protonmail.com>
2016-04-07 18:15:51 +0100

    refactor GPIO

--- fade_button.rb
+++ fade_button.rb
@@ -3,46 +3,57 @@
 require 'rubygems'
 require 'wiringpi2'
 
+# Setup Pins
+led_pins = [0, 1]
+button_pin = 8
+fade_val = 0.003
+
+# Instantiate GPIO object
+io = WiringPi::GPIO.new
+
+io.pin_mode button_pin, WiringPi::INPUT
+leds_pins.each do |pin|
+	io.pin_mode pin, WiringPi::PWM_OUTPUT
+end
+
+# Set parameters
+led[0] = io.soft_pwm_create leds_pins[0], 0, 100
+led[1] = io.soft_pwm_create leds_pins[1], 100, 100
+
 # Fade funcion
 def fade
 
-	# Setup Pins
-	led1 = 0
-	led2 = 1
-	button_pin = 8
-	fade_val = 0.003
+  # io1 = WiringPi::GPIO.new
+  # io1.pin_mode(led1, WiringPi::PWM_OUTPUT)
 
-	io1 = WiringPi::GPIO.new
-	io1.pin_mode(led1, WiringPi::PWM_OUTPUT)
+  # io2 = WiringPi::GPIO.new
+  # io2.pin_mode(led2, WiringPi::PWM_OUTPUT)
 
-	io2 = WiringPi::GPIO.new
-	io2.pin_mode(led2, WiringPi::PWM_OUTPUT)
+  # btn1 = WiringPi::GPIO.new
+  # btn1.pin_mode(button_pin, WiringPi::INPUT)
 
-	btn1 = WiringPi::GPIO.new
-	btn1.pin_mode(button_pin, WiringPi::INPUT)
+  # led1 = io1.soft_pwm_create 0, 0, 100
+  # led2 = io2.soft_pwm_create 1, 100, 100
+  
+  # Wait for button to be pressed and fade LEDs
+  
+  loop do
+    state = io.digital_read button_pin
+    if state == 0
+      # Fade
+      for led_level in 0..100
+        io.soft_pwm_write 0, led_level
+        io.soft_pwm_write 1, 100 - led_level
+        sleep fade_val
+      end
 
-	led1 = io1.soft_pwm_create 0, 0, 100
-	led2 = io2.soft_pwm_create 1, 100, 100
-	
-	# Wait for button to be pressed and fade LEDs
-	
-	loop do
-	  state = btn1.digital_read button_pin
-	  if state == 0
-	    # Fade
-	    for led_level in 0..100
-	      io1.soft_pwm_write 0, led_level
-	      io2.soft_pwm_write 1, 100 - led_level
-	      sleep fade_val
-	    end
-
-	    for led_level in 0..100
-	      io1.soft_pwm_write 0, 100 - led_level
-	      io2.soft_pwm_write 1, led_level
-	      sleep fade_val
-	    end
-	  end
-	end
+      for led_level in 0..100
+        io.soft_pwm_write 0, 100 - led_level
+        io.soft_pwm_write 1, led_level
+        sleep fade_val
+      end
+    end
+  end
 end
 
 fade
