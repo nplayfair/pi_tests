3b9a6ee414a9119078f5dc256f86fd96dafb3f9e
Nick Playfair <nplayfair@protonmaill.com>
2016-04-07 22:37:27 +0000

    Get fade ins and outs working

--- fade_button.rb
+++ fade_button.rb
@@ -37,48 +37,40 @@ def fade_off (io = io, led)
   end
 end
 
+## Fade pair
+
+def fade_pair (io = io, led1, led2)
+  for led_level in 0..100
+    io.soft_pwm_write led1, 100 - led_level
+    io.soft_pwm_write led2, led_level
+    sleep FADE_VAL
+  end
+end
+
 ## Fade funcion
 def fade (io)
 
 # Vars
 led_count = LED_PINS.length
+puts led_count
 state = 1
-on_led = 0
 
 # Wait for button to be pressed and fade LEDs
   loop do
     state = io.digital_read BUTTON_PIN
     if state == 0 # Run the routine
       # Fade first LED on
-      for led_level in 0..100
-        io.soft_pwm_write on_led, led_level
-      end
+      on_led = 0
+      fade_on io, on_led      
       # Iterate through all LEDs  
       led_count.times do |cycle|
-        # Fade on
-        for led_level in 0..100
-          led_count.times do |led|
-            io.soft_pwm_write on_led, 100 - led_level # dim on_led
-            io.soft_pwm_write on_led + 1, led_level # brighten next led 
-            sleep FADE_VAL
-          end
-        end
-        on_led = cycle
-        puts "#{cycle} \n"
+        fade_pair io, on_led, on_led + 1
+        on_led += 1
       end # end cycle
       # Fade last one out
-      for led_level in 0..100
-        io.soft_pwm_write on_led, 100 - led_level
-      end
-      puts "turn last off\n"
+      fade_off io, on_led      
     end # end if
   end # end loop
 end # end method
 
-#fade io
-fade_on io, 0
-fade_off io, 0
-fade_on io, 1
-fade_off io, 1 
-fade_on io, 2
-fade_off io, 2 
+fade io
